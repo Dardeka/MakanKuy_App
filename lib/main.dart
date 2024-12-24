1b358e910df6c042d6b450944cff5c975d70258a
OctaKoharu <syahruldwi2023@gmail.com>
2024-12-24 13:10:26 +0700

    modified main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:app_makankuy/components/olehOleh_page/create_shop.dart';
 import 'package:app_makankuy/pages/login_page.dart';
 import 'package:app_makankuy/pages/oleh2_page.dart';
+import 'package:app_makankuy/pages/profil_user.dart';
 import 'package:flutter/material.dart';
 
 import 'pages/WriterHome.dart';
